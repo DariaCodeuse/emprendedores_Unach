471e77fd9a00a7a5bf07afb4163e80c8dd507fe3
Daria <diazaguilarkarla8@gmail.com>
2023-11-16 03:26:38 -0600

    add footer to all pages

--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
+/* CUENTA */
+// Opcional: Cierra el menú cuando se hace clic en cualquier parte fuera de él
+document.addEventListener("click", function (e) {
+    const userMenu = document.getElementById("user-menu");
+    if (e.target.closest("#user-menu") !== userMenu) {
+        userMenu.querySelector(".user-menu-content").style.display = "none";
+    }
+});
+
+// Opcional: Cierra el menú al hacer clic en cualquier enlace dentro de él
+document.getElementById("user-menu").addEventListener("click", function (e) {
+    e.stopPropagation();
+    this.querySelector(".user-menu-content").style.display = "block";
+});
+
 /** carousel **/
 const wrapper = document.querySelector(".wrapper");
 const carousel = document.querySelector(".carousel");
